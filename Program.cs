e3abf109d0997fa9f7312b861fb10f043a46227f
Emir <emirkan.ulker9@gmail.com>
2023-06-18 03:10:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace faktoriyelH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, fakt=1;
+            Console.Write("Lütfen Sayıyı Giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            for(int i=sayi; i>0; i--)
+            {
+                fakt = fakt * i;
+            }
+            Console.WriteLine("Faktöriyel Sonucu: " + fakt);
+            Console.ReadLine(); 
+        }
+    }
+}
